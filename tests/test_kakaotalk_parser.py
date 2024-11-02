9e796ce68b7a0bd27f1d1197a4eef70f590e2684
gyuhyeong <kgh0730@gmail.com>
2024-11-03 00:52:46 +0900

    build: Initial tests

--- tests/test_kakaotalk_parser.py
+++ tests/test_kakaotalk_parser.py
@@ -1,33 +1,19 @@
 from time import time
 from unittest import TestCase
 
-from talkparser.kakaotalk_parser import KakaoTalkChatData
-
 
 class KakaoTalkParserTest(TestCase):
     def test_large_process_time(self):
         start = time()
-        KakaoTalkChatData(
-            path="tests/data/KakaoTalkChatsLarge.txt",
-            bot_used=True,
-            not_user=["", "방장봇", "채팅방 관리자"],
-        )
+
         print(time() - start)
 
     def test_middle_process_time(self):
         start = time()
-        KakaoTalkChatData(
-            path="tests/data/KakaoTalkChatsMiddle.txt",
-            bot_used=True,
-            not_user=["", "방장봇", "채팅방 관리자"],
-        )
+
         print(time() - start)
 
     def test_small_process_time(self):
         start = time()
-        KakaoTalkChatData(
-            path="tests/data/KakaoTalkChatsSmall.txt",
-            bot_used=True,
-            not_user=["", "방장봇", "채팅방 관리자"],
-        )
+
         print(time() - start)
