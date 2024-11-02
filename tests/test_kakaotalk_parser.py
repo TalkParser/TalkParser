44720b84cff9ac9b8b5a27a6453a78e890550c5f
gyuhyeong <kgh0730@gmail.com>
2024-11-03 04:28:00 +0900

    Feat: test code

--- tests/test_kakaotalk_parser.py
+++ tests/test_kakaotalk_parser.py
@@ -1,19 +1,40 @@
+import statistics
 from time import time
 from unittest import TestCase
+
+ITER_NUM = 100
 
 
 class KakaoTalkParserTest(TestCase):
     def test_large_process_time(self):
-        start = time()
+        process_times = []
+        for iline in range(ITER_NUM):
+            with self.subTest(line=iline):
+                start = time()
 
-        print(time() - start)
+                process_times.append(time() - start)
+                self.assertEqual(len(data.data), 1158705)
+        average_time = statistics.mean(process_times)
+        print(f"Average process time over {ITER_NUM} runs: {average_time}")
 
     def test_middle_process_time(self):
-        start = time()
+        process_times = []
+        for iline in range(ITER_NUM):
+            with self.subTest(line=iline):
+                start = time()
 
-        print(time() - start)
+                process_times.append(time() - start)
+                self.assertEqual(len(data.data), 247234)
+        average_time = statistics.mean(process_times)
+        print(f"Average process time over {ITER_NUM} runs: {average_time}")
 
     def test_small_process_time(self):
-        start = time()
+        process_times = []
+        for iline in range(ITER_NUM):
+            with self.subTest(line=iline):
+                start = time()
 
-        print(time() - start)
+                process_times.append(time() - start)
+                self.assertEqual(len(data.data), 5773)
+        average_time = statistics.mean(process_times)
+        print(f"Average process time over {ITER_NUM} runs: {average_time}")
